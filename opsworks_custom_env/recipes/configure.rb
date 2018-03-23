ff4f40a85074d7b21bc577e1fef87e1ee12f6e34
Torey Maerz <torey@rocketreferrals.com>
2018-03-23 16:23:55 -0500

    Fixes to include base env vars

--- opsworks_custom_env/recipes/configure.rb
+++ opsworks_custom_env/recipes/configure.rb
@@ -2,11 +2,12 @@
 # See https://forums.aws.amazon.com/thread.jspa?threadID=118107
 
 node[:deploy].each do |application, deploy|
+	environment_variables = deploy[:custom_env].to_h.merge(deploy[:environment_variables].to_h)
   
-  custom_env_template do
-    application application
-    deploy deploy
-    env node[:custom_env][application]
-  end
+	custom_env_template do
+		application application
+		deploy deploy
+		env environment_variables
+	end
   
 end
